79cfba27d8fb54509e0e337e5ddb7c2924abfa8a
Mahboob Hasan <mahboobhasan@Mahboobs-MacBook-Pro.local>
2026-08-21 03:00:29 +0530

    Ensure Flutter Mobile App consumes live backend catalog dynamically

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "c1cb21c4e7c71637b006835bc9530e2c",
+const RESOURCES = {"flutter_bootstrap.js": "ad163b17435914faf722482e625ebea3",
 "version.json": "148beba544507a7e2adb3382301e57c7",
 "index.html": "a5e050368519af8eb96519bbf148afbe",
 "/": "a5e050368519af8eb96519bbf148afbe",
