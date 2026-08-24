97b8b77da5530bcf8b9eed8cac12081c9a65cac9
Mahboob Hasan <mahboobhasan@Mahboobs-MacBook-Pro.local>
2026-08-24 10:57:45 +0530

    Deploy: Updated release web bundle for cosmyra.cloud

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "511afe8dd185976e330830bfae003a7c",
+const RESOURCES = {"flutter_bootstrap.js": "4dde0902841b3eead4211c4cf2574985",
 "version.json": "6dae8447f858a9f28736983416144c39",
 "index.html": "e0fb40c15340679c8e4a1967506f00b1",
 "/": "e0fb40c15340679c8e4a1967506f00b1",
-"main.dart.js": "df8cedb0f85e00cf21fd2ac7a34decd9",
+"main.dart.js": "12a43b6018cb52cade16b38b4505e591",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "997452b0a74fc34fe1dd2e6df525c294",
 "icons/Icon-192.png": "ac0987ffca71b8d1986f86a3f0e31685",
